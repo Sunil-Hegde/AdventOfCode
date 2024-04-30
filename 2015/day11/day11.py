116b6af3ac188c74df07af2a6faf85d8c51f92f6
Sunil-Hegde <sunil1hegde@gmail.com>
2024-04-30 12:28:56 +0530

    Python solution done

--- 2015/day11/day11.py
+++ 2015/day11/day11.py
@@ -1,23 +1,40 @@
-def checkTriplets(str):
+def checkTriplets(s):
     i = 0
-    while i<len(str)-1:
-        if ord(str[i+1]) == ord(str[i]) + 1 and ord(str[i+2]) == ord(str[i])+2:
+    while i < len(s) - 2:
+        if ord(s[i+1]) == ord(s[i]) + 1 and ord(s[i+2]) == ord(s[i]) + 2:
             return True
         else:
-            i+=1
+            i += 1
     return False
 
-def checkDoubles(str):
+def checkDoubles(s):
     count = 0
     i = 0
-    while i<len(str)-1:
-        z = ['i', 'o', 'l']
-        if z in str:
+    while i < len(s) - 1:
+        forbidden_chars = ['i', 'o', 'l']
+        if s[i] in forbidden_chars:
             return False
-        elif ((ord(str[i]) == ord(str[i+1])) and (ord(str[i+1]) != ord(str[i+2]))):
-            count+=1
-        i+=1
-    return (count >= 2) 
+        elif s[i] == s[i+1] and (i == len(s) - 2 or s[i+1] != s[i+2]):
+            count += 1
+        i += 1
+    return count >= 2
 
+def generatePassword(s):
+    i = True
+    while i:
+        if checkDoubles(s) and checkTriplets(s):
+            i = False
+        else:
+            index = len(s) - 1
+            while index >= 0 and s[index] == 'z':
+                index -= 1
+            if index >= 0:
+                s = s[:index] + chr(ord(s[index]) + 1) + 'a' * (len(s) - index - 1)
+            else:
+                s = 'a' + s
+    return s
 
-    
+str = "hepxcrrq"
+print("Part one: " + generatePassword(str))
+str1 = "hepxxzaa"
+print("Part two: " + generatePassword(str1))
