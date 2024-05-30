b27c84af8e08a089b19a01799d0bcf70ebea14eb
Sunil-Hegde <sunil1hegde@gmail.com>
2024-05-30 17:27:57 +0530

    updated code for day24 py

--- 2015/day24/day24.py
+++ 2015/day24/day24.py
@@ -1,4 +1,25 @@
-inputFile = '/home/sunil/Desktop/repos/AoC/2015/day24/input.txt'
+from itertools import combinations
+from functools import reduce
+
+def find_combinations(numbers, target):
+    def backtrack(start, path, target):
+        if target == 0:
+            combinations.add(tuple(sorted(path)))
+            return
+        for i in range(start, len(numbers)):
+            if numbers[i] <= target:
+                backtrack(i + 1, path + [numbers[i]], target - numbers[i])
+                
+    combinations = set()
+    backtrack(0, [], target)
+    return combinations
+
+def findSmallList(combinations):
+    min_entanglement = float('inf')
+    for combo in combinations:
+        entanglement = reduce(lambda x, y: x * y, combo)
+        min_entanglement = min(min_entanglement, entanglement)
+    return min_entanglement
 
 def processData(inputFile):
     processedData = []
@@ -10,45 +31,13 @@ def processData(inputFile):
             processedData.append(x)
     return processedData, total_sum
 
-def unique_combination(A, target, local=[], start=0):
-    result = []
-    if target == 0:
-        result.append(local)
-        return result
-
-    for i in range(start, len(A)):
-        if A[i] > target:
-            continue
-        if i > start and A[i] == A[i - 1]:
-            continue
-        result += unique_combination(A, target - A[i], local + [A[i]], i + 1)
-    return result
-
-def find_combinations(A, target):
-    A.sort()
-    return unique_combination(A, target)
-
-def findSmallList(combinations):
-    minLength = min(map(len, combinations))
-    smallList = []
-    quantumEntanglement = []
-    for i in combinations:
-        if len(i) == minLength:
-            smallList.append(i)
-    
-    for j in smallList:
-        entanglement = 1
-        for k in j:
-            entanglement*=k
-        quantumEntanglement.append(entanglement)
-    return min(quantumEntanglement)
-
+inputFile = '/home/sunil/Desktop/repos/AoC/2015/day24/input.txt'
 numberList, total_sum = processData(inputFile)
+
 target = total_sum // 3
 combinations = find_combinations(numberList, target)
-print("Part One: " + str(findSmallList(combinations)))
+print("Part One:", findSmallList(combinations))
+
 newTarget = total_sum // 4
 newCombinations = find_combinations(numberList, newTarget)
-print("Part Two: " + str(findSmallList(newCombinations)))
-
-
+print("Part Two:", findSmallList(newCombinations))
